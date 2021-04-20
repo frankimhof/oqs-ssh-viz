2651deab40d3ae144399c48855179ca0e420b495
frankimhof <imhof.frank@gmail.com>
2021-04-20 17:12:23 +0200

    datafetching from ./public and implementing a filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect, Component} from 'react';
+import Select, {OptionsType} from 'react-select';
 import './App.css';
+import Plot from './Plot';
+import {DataEntry} from './customTypes';
 
 function App() {
+  const [data, setData] = useState<DataEntry[]>([]);
+  const [useFilter, setShowFilter] = useState(false);
+  const [selectedKems, setSelectedKems] = useState<string[]>([]);
+  const [selectedSigs, setSelectedSigs] = useState<string[]>([]);
+  //const data:DataEntry[] = dataJSON.map((d:any)=>d as DataEntry);
+  useEffect(()=>{
+    const fetchData = async () =>{
+      const response = await fetch('data.json');
+      const dataFromResponse = await response.json();
+      setData(dataFromResponse as DataEntry[])
+    }
+    fetchData();
+  },[])
+
+  const handleUseFilter = () =>{
+    if(useFilter){
+      setSelectedKems(allAvailableKems);
+      setSelectedSigs(allAvailableSigs);
+    }
+    else{
+      setSelectedKems([]);
+      setSelectedSigs([]);
+    }
+    setShowFilter(!useFilter);
+  };
+
+  const handleSigFilterChange = (selectedOptions:OptionsType<{ value: string; label: string; }>) =>{
+    setSelectedSigs(selectedOptions.map(d=>d.value))
+    console.log("HANDLE SIGS")
+  }
+
+  const handleKemFilterChange = (selectedOptions:OptionsType<{ value: string; label: string; }>) =>{
+    setSelectedKems(selectedOptions.map(d=>d.value))
+    console.log("HANDLE KEMS")
+  }
+
+  const allAvailableSigs:string[]=Array.from(new Set(data.map(d => d.sigName)))
+  const allAvailableKems:string[]=Array.from(new Set(data.map(d => d.kemName)))
+  const filteredData = data.filter(d=>selectedSigs.includes(d.sigName) && selectedKems.includes(d.kemName)) 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        Openssh Speed Test Results
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+        <button onClick={handleUseFilter}>{useFilter? "Disable Filter":"Enable Filter"}</button>
+      {useFilter && 
+      <div>
+        <Select onChange={handleSigFilterChange} options={allAvailableSigs.map(sig=>({value: sig, label: sig}))} isMulti/>
+        <Select onChange={handleKemFilterChange} options={allAvailableKems.map(kem=>({value: kem, label: kem}))} isMulti/>
+      </div>
+      }
+      <div className="App-body">
+        <Plot data={filteredData}></Plot>
+      </div>
+  </div>
   );
 }
 
